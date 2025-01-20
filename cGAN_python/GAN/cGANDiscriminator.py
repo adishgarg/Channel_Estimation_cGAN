20d3a09d6ba42617684141f52e19da6dbeb9c6a5
Adish <77723061+adishgarg@users.noreply.github.com>
2025-01-20 22:47:16 +0530

    Update cGANDiscriminator.py

--- cGAN_python/GAN/cGANDiscriminator.py
+++ cGAN_python/GAN/cGANDiscriminator.py
@@ -19,39 +19,44 @@ class Discriminator(tf.keras.Model):
     def __init__(self):
         super(Discriminator, self).__init__()
         initializer = tf.random_normal_initializer(0., 0.02)
-        # downsample
+
+        # Encoder Layers (Downsampling)
         self.encoder_layer_1 = EncoderLayer(filters=64, kernel_size=4, apply_batchnorm=False) 
         self.encoder_layer_2 = EncoderLayer(filters=128, kernel_size=4)        
-        self.encoder_layer_3 = EncoderLayer(filters=128, kernel_size=4)        
+        self.encoder_layer_3 = EncoderLayer(filters=256, kernel_size=4)
 
-        # conv block1
-        self.zero_pad1 = layers.ZeroPadding2D()                                
-        self.conv = tf.keras.layers.Conv2D(512, 4, strides=1, kernel_initializer=initializer, use_bias=False)
-        self.bn1 = layers.BatchNormalization()                                 
+        # Self-Attention Block
+        self.attention = SelfAttention(filters=256)
+
+        # Intermediate Block
+        self.zero_pad1 = layers.ZeroPadding2D()
+        self.conv = layers.Conv2D(512, 4, strides=1, kernel_initializer=initializer, use_bias=False)
+        self.bn1 = layers.BatchNormalization()
         self.ac = layers.LeakyReLU()
 
-        # block2
-        self.zero_pad2 = tf.keras.layers.ZeroPadding2D()                       
-        self.last = tf.keras.layers.Conv2D(1, 4, strides=1, kernel_initializer=initializer) 
+        # Output Block
+        self.zero_pad2 = layers.ZeroPadding2D()
+        self.last = layers.Conv2D(1, 4, strides=1, kernel_initializer=initializer)
 
-    def call(self, y):
-        """inputs can be generated image. """
-        target = y
-        x = target     
+    def call(self, inputs):
+        """Pass inputs (e.g., real or generated images) through the model."""
+        x = inputs
         x = self.encoder_layer_1(x)
         x = self.encoder_layer_2(x)
         x = self.encoder_layer_3(x)
 
+        # Self-Attention Integration
+        x = self.attention(x)
+
+        # Intermediate Convolutions
         x = self.zero_pad1(x)
         x = self.conv(x)
         x = self.bn1(x)
         x = self.ac(x)
 
+        # Final Discriminator Logit
         x = self.zero_pad2(x)
-        x = self.last(x)
-        return x
-
-
+        return self.last(x)
 
 
 
